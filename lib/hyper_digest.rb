e7d1c4119e4272e62b21275c481cba5e389fdac7
carter2099 <carter2077@pm.me>
2025-09-19 16:20:20 -0400

    init

---
+++ lib/hyper_digest.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hyper_digest/version"
+
+module HyperDigest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hyper_digest/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module HyperDigest
+  VERSION = "0.1.0"
+end
