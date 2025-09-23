133e8c3e4a78a58269b6bc976c617df573eefd62
carter2099 <carter2077@pm.me>
2025-09-22 22:08:56 -0400

    add deps stop ci fails

--- spec/hyper_digest_spec.rb
+++ spec/hyper_digest_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe HyperDigest do
   it "has a version number" do
     expect(HyperDigest::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
